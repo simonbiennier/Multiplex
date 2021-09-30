ad75e51fddbb95e3b6a1603e107f99c0cbad9c35
simonbiennier <simon@biennier.fr>
2021-09-30 22:29:20 +0200

    mazystreams links

--- multiplex.py
+++ multiplex.py
@@ -1,18 +1,17 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
-url = "https://buffersports.com/football-games"
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
 headers = {"User-Agent": user_agent}
 
-req = Request(url=url, headers=headers)
-res = urlopen(req)
-data = res.read()
 
-soup = BeautifulSoup(data, "html.parser")
+def fetch_pages():
+    url = "https://buffersports.com/football-games"
+    req = Request(url=url, headers=headers)
+    res = urlopen(req)
+    data = res.read()
+    soup = BeautifulSoup(data, "html.parser")
 
-
-def find_urls():
     teams = [item.text.strip() for item in soup.select("a")]
     teams = [
         x for x in teams if ((x != "") and (x != "Link") and (("AM" and "PM") not in x))
@@ -24,5 +23,26 @@ def find_urls():
     return urls
 
 
-matches = find_urls()
-print(matches)
+def fetch_source(page_url):
+    req = Request(url=page_url, headers=headers)
+    res = urlopen(req)
+    data = res.read()
+    soup = BeautifulSoup(data, "html.parser")
+    return soup.find("iframe")["src"]
+
+
+def fetch_server(page_url):
+    source_url = fetch_source(page_url)
+    req = Request(url=source_url, headers=headers)
+    res = urlopen(req)
+    data = res.read()
+    soup = BeautifulSoup(data, "html.parser")
+    return soup.find_all("tr")[2].find("a")["href"]  # mazystreams
+
+
+servers = []
+matches = fetch_pages()
+for match in matches:
+    servers.append(fetch_server(match))
+
+print(servers)
